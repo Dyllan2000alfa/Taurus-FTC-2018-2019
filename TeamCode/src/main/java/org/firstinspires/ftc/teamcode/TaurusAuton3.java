87a172583cb4102cfd4b642ced33d6628abbc61e
arzec2004 <43833692+arzec2004@users.noreply.github.com>
2018-12-05 16:57:57 -0600

    Auton changed, 3 alternate autons added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TaurusAuton3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TaurusAuton3.java
@@ -6,11 +6,11 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
- * Created by Logan Yates on 10/13/2018.
+ * Created by Logan Yates on 12/5/2018.
  */
 
 @Autonomous
-public class TaurusAuton extends LinearOpMode{
+public class TaurusAuton3 extends LinearOpMode {
 
     TaurusHardwareMap robot = new TaurusHardwareMap();
     private ElapsedTime runtime = new ElapsedTime();
@@ -27,15 +27,6 @@ public class TaurusAuton extends LinearOpMode{
 
         //dropdown
 
-        //drive
-        while (opModeIsActive() && (runtime.seconds() < 1.0)) {
-            telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
-            telemetry.update();
-        }
-        robot.rightFrontMotor.setPower(-1);
-        robot.rightBackMotor.setPower(-1);
-        robot.leftFrontMotor.setPower(-1);
-        robot.leftBackMotor.setPower(-1);
         //color sensor/push cube
 
         //drop mech
